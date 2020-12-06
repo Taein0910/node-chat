41447b356f50525121fc59a4e56db663d7580d22
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2020-12-06 11:03:33 +0900

    socket.io fix1

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 var mongoose = require('mongoose');
 var roomName;
 
